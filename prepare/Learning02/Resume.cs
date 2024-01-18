b88e93713a2869165cf3742abd66d3d78a07519e
Steve <129707522+Chrispinsteve@users.noreply.github.com>
2024-01-17 23:14:22 -0500

    Update Resume.cs

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,1 +1,19 @@
+using System.Collections.Generic;
+using System;
 
+public class Resume
+{
+  private string _personName;
+  private List<Job> _jobs;
+
+  public Resume(string personName)
+  {
+    _personName = personName;
+    _jobs = new List<Job>();
+  }
+
+  public void AddJob(Job job)
+  {
+    _jobs.Add(job);
+  }
+}
